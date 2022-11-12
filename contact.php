98bebcf48991b250a9d4841fca44f5d210a28f34
Mohana Siddhartha Chivukula <103447836+iamsiddhu3007@users.noreply.github.com>
2022-11-12 12:26:56 +0530

    light styles
    rest will be done tonight

---
+++ contact.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact us</title>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+    </head>
+<body style="background-image: -webkit-repeating-radial-gradient(lightseagreen, powderblue, hotpink)">
+<?php  include('includes/navbar.php');?>
+
+<div style="padding-left: 90px; padding-top: 200px; padding-bottom: 200px" class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+
+    <a style="text-align: center" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Boyapati Hemanth Sai</h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400"><br>hb268@snu.edu.in <br> 2010110980</p>
+    </a>
+
+    <a style="text-align: center" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Mohana Siddhartha Chivukula</h5>
+        <p style="text-align: center" class="font-normal text-gray-700 dark:text-gray-400"><br>cs100@snu.edu.in<br>2010111021</p>
+    </a>
+
+    <a style="text-align: center" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Kolluru Jeshwanth</h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400"><br>kk345@snu.edu.in<br>2010110351</p>
+    </a>
+</div>
+</body>
+</html>
