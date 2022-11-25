f50001dc0f5f471a91ce1ba61b3f8261ad49d7d6
Mohana Siddhartha Chivukula <103447836+iamsiddhu3007@users.noreply.github.com>
2022-11-25 17:45:47 +0530

    25.11

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,30 @@
+<?php
+session_start();
+error_reporting(0);
+include('../includes/config.php');
+?>
 <html>
     <head>
-    <title>The Holiday Destination</title>
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+        <title>Admin</title>
     </head>
     <body>
-        
+<?php include('../includes/navbar.php');
+?>
+
+            <?php
+            echo "hi";
+            $sql0="select * from package where Package_id=1";
+            echo "hi";
+            $query99=dbh->prepare($sql0);
+            echo "hi";
+            $query99->execute();
+            $result0=$query99->fetch(PDO::FETCH_OBJ);
+            echo $result0;
+            ?>
+
+
+
+            <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
     </body>
-</html>
+    </html>
