f563575f29fc3b60a829154bb1b1514ddb42b2dc
Mohana Siddhartha Chivukula <103447836+iamsiddhu3007@users.noreply.github.com>
2022-11-27 01:53:22 +0530

    admin basically done

---
+++ admin/newhotelroom.php
@@ -0,0 +1,151 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<DOCTYPE HTML>
+    <html>
+    <head>
+        <title>New Package</title>
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+    </head>
+    <body>
+    <?php include('includes/navbar.php');?>
+    <?php include('includes/sidebar.php'); ?>
+    <div class="text-center">
+        <button class="text-white bg-gray-900 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="button" data-drawer-target="drawer-disable-body-scrolling" data-drawer-show="drawer-disable-body-scrolling" data-drawer-body-scrolling="false" aria-controls="drawer-disable-body-scrolling">
+            Menu
+        </button>
+    </div>
+    <div>
+        <div style="float: left; padding-left: 3%; padding-top: 2%;" class="overflow-x-auto relative">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="py-3 px-6">
+                        Room_id
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Occupancy
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Price
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Hotel_id
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Type
+                    </th>
+                    <th scope="col" class="py-3 px-6">
+                        Facilities
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php
+                $sql = "SELECT * from room";
+                $query = $dbh->prepare($sql);
+                $query->execute();
+                $results=$query->fetchAll(PDO::FETCH_OBJ);
+                $cnt=1;
+                //            echo('Package_id,    Name,   Location_id');
+
+
+                if($query->rowCount() > 0)
+                {
+                    foreach($results as $result)
+                    {	?>
+                        <div>
+
+                            <!--                        --><?php //echo htmlentities($result->Package_id);?>
+                            <!--                        --><?php //echo htmlentities($result->Name);?>
+                            <!--                        --><?php //echo htmlentities($result->Location_id);?>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <?php echo htmlentities($result->Room_id);?>
+                                </th>
+                                <td class="py-4 px-6">
+                                    <?php echo htmlentities($result->Occupancy);?>
+                                </td>
+                                <td class="py-4 px-6">
+                                    <?php echo htmlentities($result->Price);?>
+                                </td>
+                                <td class="py-4 px-6">
+                                    <?php echo htmlentities($result->Hotel_id);?>
+                                </td>
+                                <td class="py-4 px-6">
+                                    <?php echo htmlentities($result->Type);?>
+                                </td>
+                                <td class="py-4 px-6">
+                                    <?php echo htmlentities($result->Facilities);?>
+                                </td>
+                            </tr>
+
+                        </div>
+
+                    <?php }} ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <?php
+    if(isset($_POST['submit'])) {
+        $Occupancy = $_POST['Occupancy'];
+        $Price = $_POST['Price'];
+        $Hotel_id = $_POST['Hotel_id'];
+        $Type = $_POST['Type'];
+        $Facilities = $_POST['Facilities'];
+        $Name = $_POST['Name'];
+        $sql1 = "insert into room(Occupancy, Price, Hotel_id, Type, Facilities) values ('$Occupancy', '$Price','$Hotel_id', '$Type','$Facilities')";
+        $query1 = $dbh->prepare($sql1);
+        $query1->execute();
+    }
+    ?>
+
+
+    <div class="mt-3" style="position: absolute;padding-left: 1100px;padding-right: 20 px; float: right">
+        <p class="text-lg text-gray-900 dark:text-white">Give the following to insert into the database</p>
+        <form  method="POST" action="">
+            <div>
+
+                <div class="mb-6">
+                    <label for="Occupancy" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Occupancy</label>
+                    <input type="number" id="Occupancy" name="Occupancy" placeholder="Enter Occupancy" required min="1" max="100" class="bg-gray-50 border border-gray-300 w-48 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
+                <div class="mb-6">
+                    <label for="Price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Price</label>
+                    <input type="number" id="Price" name="Price" placeholder="Enter Price" required min="1"  class="bg-gray-50 border border-gray-300 w-48 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
+                <div class="mb-6">
+                    <label for="Hotel_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Hotel_id</label>
+                    <input type="number" id="Hotel_id" name="Hotel_id" placeholder="Enter Hotel_id" required min="1"  class="bg-gray-50 border border-gray-300 w-48 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                </div>
+                <div class="mb-6">
+                    <label for="Type" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Type</label>
+                    <input type="name" name="Type" id="Type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                </div>
+                <div class="mb-6">
+                    <label for="Facilities" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Facilities</label>
+                    <input type="name" name="Facilities" id="Facilities" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                </div>
+
+            </div>
+
+            <br>
+
+            <input type="submit" name="submit" value="submit" class="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+
+        </form>
+
+    </div>
+
+
+
+    </div>
+
+    <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+    </body>
+    </html>
